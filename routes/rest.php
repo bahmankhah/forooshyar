1f6dd963c616dbdcef4160d1e3bb860afaac9ddd
Hesam <bahmankhah1@gmail.com>
2025-12-25 17:24:14 +0330

    adding middleware

--- routes/rest.php
+++ routes/rest.php
@@ -6,14 +6,14 @@ use WPLite\Facades\Route;
 
 Route::rest(function ($router) {
     // Main products endpoint - GET /products
-    $router->get('/products', [ProductController::class, 'index'])->middleware([ApiLoggingMiddleware::class])->make();
+    $router->get('/products', [ProductController::class, 'index'])->middleware(ApiLoggingMiddleware::class)->make();
     
     // Single product endpoint - GET /products/{id}
-    $router->get('/products/{id}', [ProductController::class, 'show'])->middleware([ApiLoggingMiddleware::class])->make();
+    $router->get('/products/{id}', [ProductController::class, 'show'])->middleware(ApiLoggingMiddleware::class)->make();
     
     // Products by IDs - POST /products/by-ids
-    $router->post('/products/by-ids', [ProductController::class, 'getByIds'])->middleware([ApiLoggingMiddleware::class])->make();
+    $router->post('/products/by-ids', [ProductController::class, 'getByIds'])->middleware(ApiLoggingMiddleware::class)->make();
     
     // Products by slugs - POST /products/by-slugs
-    $router->post('/products/by-slugs', [ProductController::class, 'getBySlugs'])->middleware([ApiLoggingMiddleware::class])->make();
+    $router->post('/products/by-slugs', [ProductController::class, 'getBySlugs'])->middleware(ApiLoggingMiddleware::class)->make();
 });
