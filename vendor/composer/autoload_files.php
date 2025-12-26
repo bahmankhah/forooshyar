083da6fb89c9a7e05131924737242ab351817f24
Hesam <bahmankhah1@gmail.com>
2025-12-26 22:38:25 +0330

    added woocommerce schdeuler

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,9 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
-    '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
     '8ed042297ae8365df778865d8666ca4e' => $vendorDir . '/hsm/wplite/src/Helpers/main.php',
-    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
     'b2f834a1b3bd09787aa228b8e4d58b3e' => $baseDir . '/src/WPLite/Helpers/main.php',
 );
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,11 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'WPLite\\' => array($vendorDir . '/hsm/wplite/src'),
-    'Tests\\' => array($baseDir . '/tests'),
-    'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
-    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'Forooshyar\\' => array($baseDir . '/src'),
-    'Faker\\' => array($vendorDir . '/fakerphp/faker/src/Faker'),
-    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
-    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
